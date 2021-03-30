11ebc0e65bdd5fa19291a0fbe6e00c3914feb5c0
unknown <akshat.aksharma@gmail.com>
2021-03-30 15:11:21 +0530

    Few more funtions tested

--- panda.py
+++ panda.py
@@ -7,3 +7,27 @@ print(dataframe.info())
 print(dataframe.columns)
 basepay = dataframe['BasePay']
 print(f'The Mean is {basepay.mean()}')
+#find the highest amount of overtime pay 
+overtime = dataframe['OvertimePay'].max()
+print(f'the highest overtime is {overtime}')
+## all information about an employee
+details_emp = [dataframe[dataframe['EmployeeName'] == 'CATHRINE SNEED']]
+print(details_emp)
+#how much dose she make 
+details_sal = dataframe[dataframe['EmployeeName'] == 'CATHRINE SNEED']['TotalPay']
+print(f'her totle pay is {details_sal}')
+#what is the name of the highest paid person 
+highest_paid = dataframe['TotalPayBenefits'].max()
+print(dataframe[dataframe['TotalPayBenefits'] == highest_paid]['EmployeeName'])
+# or what we can do is 
+print(dataframe.loc[dataframe['TotalPayBenefits'].idxmax()])
+# now find the lowest paid person in the company 
+lowest = dataframe.loc[dataframe['TotalPayBenefits'].idxmin()]
+print(f'the lowest payed person is {lowest}')
+# avg basepay for all the employess per year 
+print(dataframe.groupby('Year').mean()['BasePay'])
+# how many unique job titles are there in the company 
+print(dataframe['JobTitle'].nunique())
+#what are teh top 5 most common jobs 
+commonjobs = dataframe['JobTitle'].value_counts().head(5)
+print(commonjobs)
