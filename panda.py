3b7258e15b82333a1faaeb1fe6876a3190361af0
unknown <akshat.aksharma@gmail.com>
2021-03-30 13:34:08 +0530

    second commit

--- panda.py
+++ panda.py
@@ -3,3 +3,7 @@ dataframe = pd.read_csv('Salaries.csv')
 #print(dataframe) -> to check if the read was successful 
 #print(dataframe.head(1))
 print(dataframe.info())
+#now find the average basepay 
+print(dataframe.columns)
+basepay = dataframe['BasePay'].mean()
+print(basepay)
