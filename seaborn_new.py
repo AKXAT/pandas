5a85d518c6a212cd16b56afa056c6dbe38c94510
AKXAT <akshat.aksharma@gmail.com>
2021-04-06 15:00:15 +0530

    Grids

--- seaborn_new.py
+++ seaborn_new.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 #Seaborn has some inbuild datasets which you can load
 tips = sns.load_dataset('tips')
-print(tips.head())
+flights = sns.load_dataset('flights')
+iris = sns.load_dataset('iris')
+#print(tips.head())
 # - Lets see the first plot type DIST PLOT. Its a way to see a uni-varaible distribution.
 sns.displot(tips['total_bill'],kde='True')
 # - Let's see Joint Plot, here we can combint two DIST plots
@@ -26,14 +28,23 @@ sns.stripplot(x='day',y='total_bill',data=tips,jitter=True,hue='sex',dodge=True)
 sns.swarmplot(x='day',y='total_bill',data=tips)
 # - Let's see about **FACTOR PLOT**, this is general form of all these plots
 sns.catplot(x='day',y='total_bill',data=tips,kind='bar')
-plt.show()
 # - Let's First create set up or data input on which we are going to plot the MATRIX PLOTS 
-tips = sns.load_dataset('tips')
-flights = sns.load_dataset('flights')
 tc = tips.corr()
 sns.heatmap(tc,annot=True,cmap='coolwarm')
 fm = flights.pivot_table(index='month',columns='year',values='passengers')
 sns.heatmap(fm,linecolor='white',linewidth='1')
 # - Let's now see how a **CLUISTER MAP** looks like. It's basically cluster of Heat Map
 sns.clustermap(fm)
+# REGRESSION PLOT 
+sns.lmplot(x='total_bill',y='tip',data=tips,hue='sex',markers=['o','v'])
+# GRID
+var = sns.PairGrid(iris)
+var.map(plt.scatter)
+# if you want to specify what to mention on the diagonal 
+var.map_diag(sns.distplot)
+var.map_upper(plt.scatter)
+var.map_lower(sns.kdeplot)
+# Facet Grid 
+g = sns.FacetGrid(data=tips,col='time',row='smoker')
+g.map(sns.displot,'total_bill')
 plt.show()
